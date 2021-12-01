3bc855302dda8dfb79eaf1894b4c355b14040be8
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2021-12-01 21:41:45 +0000

    new functions added and moved

--- play.js
+++ play.js
@@ -1,7 +1,9 @@
 const net = require("net");
 
 const {connect} = require("./client");
-
+const {setupInput} = require("./input");
 
 console.log("Connecting ...");
 connect();   // Activating the connect function in client.js
+
+setupInput();
