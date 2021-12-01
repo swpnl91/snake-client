e643422303226bb35af61749e89c775679b10692
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2021-12-01 20:46:53 +0000

    adding after removing connect function

--- play.js
+++ play.js
@@ -1,31 +1,7 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: 50541 // PORT number here,
-  });
+const {connect} = require("./client");
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    conn.write('Snake charmer in the house.. Yo!'); // code that does something when the connection is first established
-  });
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  /*conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  }); */
-  
-
-
-  return conn;
-};
 
 console.log("Connecting ...");
-connect();
+connect();   // Activating the connect function in client.js
